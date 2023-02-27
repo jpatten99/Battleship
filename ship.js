f39868dbd459a09fb0ee0d0b5f8baca078a996bd
Joshua <joshuagpatten@gmail.com>
2023-02-27 17:00:10 -0500

    change class to factory function

--- ship.js
+++ ship.js
@@ -1,27 +1,37 @@
-class Ship {
+// export class Ship {
 
-  constructor(shipLength) {
-    this.shipLength = shipLength;
-    this.timesHit = 0;
-  }
+//   constructor(shipLength) {
+//     this.shipLength = shipLength;
+//     this.timesHit = 0;
+//   }
 
-  sayHi() {
-    console.log(this);
-  }
+//   sayHi() {
+//     console.log(this);
+//   }
 
-  hit() {
-    this.timesHit += 1;
-  }
+//   hit() {
+//     this.timesHit += 1;
+//   }
 
-  isSunk() {
-    return (this.shipLength === this.timesHit);
-  }
+//   isSunk() {
+//     return (this.shipLength === this.timesHit);
+//   }
 
+// }
+
+export function shipFactory(shipLength) {
+  return {
+    shipLength,
+    timesHit: 0,
+    hit() {
+      // eslint-disable-next-line no-plusplus
+      this.timesHit++;
+    },
+    isSunk() {
+      return (this.shipLength <= this.timesHit);
+    },
+  };
 }
 
 // Usage:
-export const ship = new Ship(3);
-export const ship2 = new Ship(6);
-ship.hit();
-ship.hit();
-ship.hit();
+export const testShip = shipFactory(3);
