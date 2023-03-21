650a1aee7bfa07fd108d3c16844592d84f3df703
Joshua <joshuagpatten@gmail.com>
2023-03-21 16:02:17 -0400

    Add placeShipRandom function

--- player.js
+++ player.js
@@ -1,3 +1,5 @@
+import {gameBoardFactory} from "./gameboard.js";
+
 // eslint-disable-next-line import/prefer-default-export
 export function playerFactory(playerName = "computer") {
   // max > output >= min
@@ -18,8 +20,31 @@ export function playerFactory(playerName = "computer") {
         x = getRandomArbitrary(0, 7);
         validPair = gameBoard.receiveAttack(y, x);
       }
-      console.log(`x: ${x} y: ${y}`);
-      return y;
     },
+    placeShipRandom(gameboard, shipName, shipLength) {
+      const direction = getRandomArbitrary(1, 3);
+      if(direction===1){
+        let y = getRandomArbitrary(0, 7);
+        let x = getRandomArbitrary(0, 7);
+        let flag = gameboard.placeShipHorizontally(y, x, shipLength, shipName);
+        while(flag !== "success") {
+          y = getRandomArbitrary(0, 7);
+          x = getRandomArbitrary(0, 7);
+          flag = gameboard.placeShipHorizontally(y, x, shipLength, shipName);
+        }
+        return "success";
+      }
+      
+      let y = getRandomArbitrary(0, 7);
+      let x = getRandomArbitrary(0, 7);
+      let flag = gameboard.placeShipVertically(y, x, shipLength, shipName);
+      while(flag !== "success") {
+        y = getRandomArbitrary(0, 7);
+        x = getRandomArbitrary(0, 7);
+        flag = gameboard.placeShipVertically(y, x, shipLength, shipName);
+      }
+      return "success";
+      
+    }
   };
 }
