8f0249dc60f03cadd060cefce164839e39798108
Joshua <joshuagpatten@gmail.com>
2023-03-22 17:03:46 -0400

    Move script to bottom

--- DOMInteraction.js
+++ DOMInteraction.js
@@ -3,56 +3,56 @@ import {onClickFunction} from "./gameLogic.js";
 
 // eslint-disable-next-line import/prefer-default-export
 export const renderGameBoards = (inputArray1, inputArray2) => {
-  const myGameBoard = document.getElementById("leftGameBoardContainer");
-  myGameBoard.innerHTML = "";
+  const leftGameBoard = document.getElementById("leftGameBoardContainer");
+  leftGameBoard.innerHTML = "";
   for(let i=0; i<7; i+=1) {
     for(let j=0; j<7; j+=1) {
       const myDiv = document.createElement('div');
       switch(inputArray1[i][j]) {
         case null:
-          myDiv.style.backgroundColor = "gray";
+          myDiv.style.backgroundColor = "black";
           break;
         case "miss":
-          myDiv.style.backgroundColor = "red";
+          myDiv.style.backgroundColor = "blue";
           myDiv.innerText = "miss";
           break;
           case "hit":
-            myDiv.style.backgroundColor = "green";
+            myDiv.style.backgroundColor = "red";
             myDiv.innerText = "hit";
             break;  
         default:
-          myDiv.style.backgroundColor = "black";
+          myDiv.style.backgroundColor = "gray";
       }
-      myGameBoard.appendChild(myDiv);
+      leftGameBoard.appendChild(myDiv);
     }
   }
-  const computerGameBoard2 = document.getElementById("rightGameBoardContainer");
-  computerGameBoard2.innerHTML = "";
+  const rightGameBoard = document.getElementById("rightGameBoardContainer");
+  rightGameBoard.innerHTML = "";
   for(let i=0; i<7; i+=1) {
     for(let j=0; j<7; j+=1) {
       const myDiv = document.createElement('div');
       switch(inputArray2[i][j]) {
         case null:
-          myDiv.style.backgroundColor = "gray";
+          myDiv.style.backgroundColor = "black";
           myDiv.onclick = () => {
             onClickFunction(i, j);
           };
           break;
         case "miss":
-          myDiv.style.backgroundColor = "red";
+          myDiv.style.backgroundColor = "blue";
           myDiv.innerText = "miss";
           break;
           case "hit":
-            myDiv.style.backgroundColor = "green";
+            myDiv.style.backgroundColor = "gray";
             myDiv.innerText = "hit";
             break;  
         default:
-          myDiv.style.backgroundColor = "gray";
+          myDiv.style.backgroundColor = "black";
           myDiv.onclick = () => {
             onClickFunction(i, j);
           };
       }
-      computerGameBoard2.appendChild(myDiv);
+      rightGameBoard.appendChild(myDiv);
     }
   }
 };
