e82c34fd494b63cd4928c160d33247afe92d0c37
Joshua <joshuagpatten@gmail.com>
2023-03-22 17:08:01 -0400

    Change ship size limit

--- player.js
+++ player.js
@@ -22,29 +22,72 @@ export function playerFactory(playerName = "computer") {
       }
     },
     placeShipRandom(gameboard, shipName, shipLength) {
+      let attempts = 0;
       const direction = getRandomArbitrary(1, 3);
       if(direction===1){
         let y = getRandomArbitrary(0, 7);
         let x = getRandomArbitrary(0, 7);
         let flag = gameboard.placeShipHorizontally(y, x, shipLength, shipName);
+        attempts += 1;
         while(flag !== "success") {
           y = getRandomArbitrary(0, 7);
           x = getRandomArbitrary(0, 7);
           flag = gameboard.placeShipHorizontally(y, x, shipLength, shipName);
+          attempts += 1;
+          if(attempts === 50){
+            let flag2 = "nope";
+            for(let i = 0; i < 7; i++) {
+              for(let j = 0; j < 7; j++) {
+                flag2 = gameboard.placeShipHorizontally(i, j, shipLength, shipName);
+                if(flag2 === "success"){
+                  return "success";
+                }
+              }
+            }
+            for(let i = 0; i < 7; i++) {
+              for(let j = 0; j < 7; j++) {
+                flag2 = gameboard.placeShipVertically(i, j, shipLength, shipName);
+                if(flag2 === "success"){
+                  return "success";
+                }
+              }
+            }
+            return "failed";
+          }
         }
         return "success";
       }
-      
       let y = getRandomArbitrary(0, 7);
       let x = getRandomArbitrary(0, 7);
       let flag = gameboard.placeShipVertically(y, x, shipLength, shipName);
+      attempts += 1;
       while(flag !== "success") {
         y = getRandomArbitrary(0, 7);
         x = getRandomArbitrary(0, 7);
         flag = gameboard.placeShipVertically(y, x, shipLength, shipName);
+        attempts += 1;
+        if(attempts === 50){
+          let flag2 = "nope";
+            for(let i = 0; i < 7; i++) {
+              for(let j = 0; j < 7; j++) {
+                flag2 = gameboard.placeShipVertically(i, j, shipLength, shipName);
+                if(flag2 === "success"){
+                  return "success";
+                }
+              }
+            }
+            for(let i = 0; i < 7; i++) {
+              for(let j = 0; j < 7; j++) {
+                flag2 = gameboard.placeShipHorizontally(i, j, shipLength, shipName);
+                if(flag2 === "success"){
+                  return "success";
+                }
+              }
+            }
+          return "failed";
+        }
       }
       return "success";
-      
     }
   };
 }
